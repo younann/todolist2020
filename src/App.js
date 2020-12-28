b45aef8957ce2f892fa012d56ce86ea0c69784f0
younann <younan.n@gmail.com>
2020-12-28 14:02:19 +0200

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,45 @@
+import React, { Component } from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Takeout the trash',
+        completed: false,
+      },
+      {
+        id: 2,
+        title: 'write some code',
+        completed: false,
+      },
+      {
+        id: 3,
+        title: 'dinner with wife',
+        completed: false,
+      },
+    ],
+  };
+
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map((todo) => {
+        if (todo.id == id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      }),
+    });
+  };
+  render() {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} markComplete={this.markComplete} />
+      </div>
+    );
+  }
 }
 
 export default App;
